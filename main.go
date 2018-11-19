1989e0ef97fd56e2d087b695135fe65a060f74db
pantheonsh <pantheonshell@yahoo.com>
2018-11-19 18:25:55 -0200

    Servidor TCP feito

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
+var debug = false
+var addr = ":1337"
+
 func main() {
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("Erro ao iniciar o servidor.")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Servidor HTTP está rodando sobre o endereço " + addr)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+
+		} else {
+			handleConnection(conn)
+		}
+	}
+}
+
+func handleConnection(conn net.Conn) {
 
 }
