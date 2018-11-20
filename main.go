ea0223dfcc0d5a9fc9bb2828126498873af999b6
pantheonsh <pantheonshell@yahoo.com>
2018-11-20 14:30:51 -0200

    Implementar diretórios relativos

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 var debug = true
 var addr = ":1337"
+var htdocs = "htdocs"
 
 func main() {
-	http.HandleFunc("/", handleConnection)
-	http.ListenAndServe(addr, nil)
+	cwd, _ := os.Getwd()
+	servePath := filepath.Join(cwd, htdocs)
+	_ = os.Mkdir(servePath, os.ModePerm)
+
+	println("Servindo arquivos a partir de " + servePath)
 }
 
 func handleConnection(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +24,7 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 		fmt.Print(r.RemoteAddr, r.Method)
 	}
 
-	w.Header().Add("X-Server", "Cacau")
+	w.Header().Add("X-File-Server", "Cacau (Golang)")
 	w.WriteHeader(200)
 
 	fmt.Fprintf(w, "Olá, mundo!")
